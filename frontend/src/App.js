a43df8add1165897077f3d07a3a5271c8be0caf4
Dhruv Dhari <dhruvdhari2003@gmail.com>
2023-09-08 12:43:14 +0530

    added private route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,44 @@
 import "./output.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginComponent from "./routes/Login";
 import SignupComponent from "./routes/Signup";
 import HomeComponent from "./routes/Home";
+import { Cookies, useCookies } from "react-cookie";
 
 function App() {
+  const [cookie, setCookie] = useCookies(["token"]);
+  console.log(cookie.token);
+
   return (
     <div className="w-screen h-screen ">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HelloComponent/>}/>
-          <Route path="/login" element={<LoginComponent />} />
-          <Route path="/signup" element={<SignupComponent/>} />
-          <Route path="/home" element={<HomeComponent/>} />
-          
-        </Routes>
+        {cookie.token ? (
+          <Routes>
+            <Route path="/" element={<HelloComponent />} />
+
+            <Route path="/home" element={<HomeComponent />} />
+
+            <Route path="*" element={<Navigate to="/home" />} />
+          </Routes>
+        ) : (
+          <Routes>
+            <Route path="/home" element={<HomeComponent />} />
+
+            <Route path="/login" element={<LoginComponent />} />
+
+            <Route path="/signup" element={<SignupComponent />} />
+
+            <Route path="*" element={<Navigate to="/login" />} />
+
+          </Routes>
+        )}
       </BrowserRouter>
     </div>
   );
 }
 
-const HelloComponent=()=>{
+const HelloComponent = () => {
   return <div>Ye hai hello component</div>;
-}
+};
 
 export default App;
