14dbd543ab1d69a838fd05feb655dd8b69fc9a92
Dhruv Dhari <dhruvdhari2003@gmail.com>
2023-09-08 22:07:35 +0530

    done for todayy

--- frontend/src/routes/UploadSong.js
+++ frontend/src/routes/UploadSong.js
@@ -69,12 +69,14 @@ const UploadSong=()=>{
                         Upload Your Music
                     </div>
                     <div className=" w-2/3 flex space-x-3">
-                        <div className="w-1/2"><TextInput/></div>
-                        <div className="w-1/2"> <TextInput/></div>
+                        <div className="w-1/2"><TextInput label="Name" labelClassName="text-white"
+                        placeholder={"Name"}/></div>
+                        <div className="w-1/2"> <TextInput label="Thumbnail" labelClassName="text-white"
+                        placeholder={"Thumbnail"}/></div>
                         
                        
                     </div>
-                        <TextInput/>
+                     
 
                 </div>
 
