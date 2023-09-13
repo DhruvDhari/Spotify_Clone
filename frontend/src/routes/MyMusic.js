970fdccb6a1c380d3cd8f4512dd7936448199d19
Dhruv Dhari <dhruvdhari2003@gmail.com>
2023-09-13 16:02:38 +0530

    changed mymusic component

--- frontend/src/routes/MyMusic.js
+++ frontend/src/routes/MyMusic.js
@@ -1,122 +1,36 @@
 import spotify_logo from "../assets/images/logo.svg";
 import IconText from "../components/shared/IconText";
 import { Icon } from "@iconify/react";
-import {Howl,Howler} from "howler";
+import { Howl, Howler } from "howler";
 import TextWithHover from "../components/shared/TextWithHover";
 import SingleSongCard from "../components/shared/SingleSongCard";
 import { useEffect, useState } from "react";
 import { makeAuthenticatedGETRequest } from "../utils/serverHelpers";
+import LoggedInContainer from "../containers/LoggedInContainer";
 
-const MyMusic=()=>{
-
+const MyMusic = () => {
     const[songData,setSongData]=useState([]);
 
-    const[soundPlayed,setSoundPlayed] = useState(null);
-
-    const playSound =(songSrc)=>{
-        if(soundPlayed){
-            soundPlayed.stop();
-        }
-        let sound=new Howl({
-            src:[songSrc],
-            html5:true
-        });
-        setSoundPlayed(sound)
-
-
-        sound.play();
-       
-    };
-    
-
     useEffect(()=>{
-        const getData=async ()=>{
-            const response=await makeAuthenticatedGETRequest("/song/get/mysongs");
-            setSongData(response.data);
-        };
-        getData();
-       
-    },[]);
+                const getData=async ()=>{
+                    const response=await makeAuthenticatedGETRequest("/song/get/mysongs");
+                    setSongData(response.data);
+                };
+                getData();
+        
+            },[]);
 
-    return(
-        <>
-        <div className="h-full w-full flex">
-            <div className="left h-full w-1/5 bg-black flex flex-col justify-between pb-10">
-                <div>
-                <div className="logoDiv p-6">
-                    <img src={spotify_logo} alt="Spotify logo" width={125} />
-                </div>
+  return (
+    <LoggedInContainer>
+      <div className="text-white text-xl font-semibold pb-4 pl-2 pt-8">My Songs</div>
 
-                <div className="py-4">
-                <IconText iconName={"material-symbols:home"} displayText={"Home"} />
-                <IconText iconName={"material-symbols:search-rounded"} displayText={"Search"}/>
-                <IconText iconName={"icomoon-free:books"} displayText={"Library"}/>
-                <IconText iconName={"material-symbols:library-music-sharp"} displayText={"My Music"}  active/>
-                </div>
-
-                <div className="pt-4">
-                <IconText iconName={"material-symbols:add-box"} displayText={"Create Playlist"}/>
-                <IconText iconName={"mdi:cards-heart"} displayText={"Liked Songs"}/>
-                </div>
-
-                </div>
-
-                <div className="px-4">
-                <div className="text-white border border-gray-100 w-2/5 flex px-2 py-1 rounded-full items-center justify-center hover:border-white cursor-pointer"  >
-                    <Icon icon="carbon:earth-europe-africa"/>
-                <div className="ml-2 text-sm font-semibold">
-                English
-                </div>
-                </div>
-                </div>
-              
-                
-
-            </div>
-
-            <div className="right h-full w-4/5 bg-app-black overflow-auto">
-                <div className="navbar w-full h-1/10 bg-black bg-opacity-30 flex items-center justify-end">
-                    <div className="w-1/2 flex h-full ">
-                        <div className="w-2/3 flex justify-around items-center">
-                        <TextWithHover displayText={"Premium"}/>
-                    <TextWithHover displayText={"Support"}/>
-                    <TextWithHover displayText={"Download"}/>
-                    <div className="h-1/2 border border-white "></div>
-                        </div>
-                    
-                    <div className="w-1/3 flex justify-around h-full items-center">
-                    <TextWithHover displayText={"Upload Song "}/>
-                    <div className=" bg-white w-10 h-10 flex items-center justify-center rounded-full font-semibold cursor-pointer">
-                        DD
-                    </div>
-                    </div>
-                    
-                    </div>
-                 
-                   
-                </div>
-
-                <div className="content p-8 overflow-auto">
-
-                    <div className="text-white text-xl font-semibold pb-4 pl-2">My Songs</div>
-
-                    <div className="space-y-2 overflow-auto">
-                    {songData.map((item)=>{
-                        return <SingleSongCard info={item} playSound={playSound} />
-                    })}
-                    </div>
-                    
-                </div>
-
-
-            </div>
-        </div>
-        </>
-    );
+      <div className="space-y-2 overflow-auto">
+        {songData.map((item) => {
+          return <SingleSongCard info={item} playSound={()=>{}} />;
+        })}
+      </div>
+    </LoggedInContainer>
+  );
 };
-
-
-
-
 
 export default MyMusic;
